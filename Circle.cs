a71f049d81b8f07a174c407dca1a8be40ae3f597
Nguyen HoangTung <tung05122000@gmail.com>
2024-03-06 13:21:27 +0700

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+namespace a1
+
+    {
+        public class Circle
+        {
+
+            public int Radius { get; set; }
+
+
+            public double Area { get; set; }
+
+            public double Circumference { get; set; }
+        }
+    }
---
+++ Controllers/CircleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace a1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircleController : Controller
+    {
+        [HttpGet]
+        public ActionResult Calculate(double radius)
+        {
+            var circumference = 2 * Math.PI * radius;
+            var area = Math.PI * radius * radius;
+
+            var result = new
+            {
+                Circumference = circumference,
+                Area = area
+            };
+
+            return Json(result);
+        }
+    }
+}
